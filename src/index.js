03dff77c4ee8e4785cd32b7affc6638ee91f6fb3
varun kaushik <varun0509kaushik@gmail.com>
2024-08-03 12:30:08 +0530

    Events

--- src/index.js
+++ src/index.js
@@ -11,17 +11,15 @@ const publicDirectoryPath=path.join(__dirname,'../public')
 const port=process.env.PORT||3000
 
 app.use(express.static(publicDirectoryPath))
-let count=0
 
 io.on('connection',(socket)=>{
     console.log('New websocket connection')
-    socket.emit('countUpdated',count)
-    socket.on('increment',()=>{
-        count++
-        //socket.emit('countUpdated',count)//emit to a single connection
-        io.emit('countUpdated',count)
+    socket.emit('message','Welcome!')
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message)
     })
 })
+
 server.listen(port,()=>{
     console.log(`Server is set up on port ${port}!`)
 })
