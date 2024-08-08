7edcef9961d0e47120f0647128d767bcfba3f707
varun kaushik <varun0509kaushik@gmail.com>
2024-08-08 14:37:01 +0530

    Rendering Messages

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ io.on('connection',(socket)=>{//when a client is connected
         io.emit('message',message)//emit all
         callback()
     })
-
+    
     socket.on('sendLocation',(coords,callback)=>{
-        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         //io.emit('message',`Location: ${coords.latitude}, ${coords.longitude}`)
         callback()
     })
